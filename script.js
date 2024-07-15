7053c4fb4355b9893ca6d27d36737389c748b271
officialpaaqwasi <iklutse@st.ug.edu.gh>
2024-07-15 16:56:18 +0000

    phase 2

---
+++ script.js
@@ -0,0 +1,42 @@
+let products = {
+data: [
+{
+   productName: "Regular White T-Shirt",
+   category: "Topwear",
+   price: "36",
+   image: "white-tshirt.jpg",
+ },
+ {
+   productName: "Beige Short Skirt",
+   category: "Bottomwear",
+   price: "45",
+   image: "white-tshirt.jpg",
+ },
+ {
+   productName: "Sporty SmartWatch",
+   category: "Watch",
+   price: "99",
+   image: "sporty-smartwatch.jpg",
+ },
+ {
+   productName: "Basic Knitted Top",
+   category: "Topwear",
+   price: "29",
+   image: "knitted-top.jpg",
+ },
+ {
+   productName: "Black Leather Jacket",
+   category: "Jacket",
+   price: "129",
+   image: "black-leather-jacket.jpg",
+ },
+ {
+   productName: "Stylish Pink Trousers",
+   category: "Bottomwear",
+   price: "129",
+   image: "white-tshirt.jpg",
+ },
+
+]
+ 
+}
