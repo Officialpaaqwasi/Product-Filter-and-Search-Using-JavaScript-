7a3711a97998b7deea07a5555a93753154a21bf9
officialpaaqwasi <iklutse@st.ug.edu.gh>
2024-07-16 03:00:22 +0000

    16/07/24 Phase 1

--- script.js
+++ script.js
@@ -33,10 +33,102 @@ data: [
  {
    productName: "Stylish Pink Trousers",
    category: "Bottomwear",
-   price: "129",
-   image: "white-tshirt.jpg",
+   price: "89",
+   image: "pink-trousers.jpg",
+ },
+ {
+   productName: "Brown Men's Jacket",
+   category: "Jacket",
+   price: "189",
+   image: "brown-jacket.jpg",
+ },
+ {
+   productName: "Comfy Gray Pants",
+   category: "Bottomwear",
+   price: "49",
+   image: "comfy-gray-pants.jpg",
  },
 
-]
+],
  
+}
+
+for(let i of products.data){
+    // Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially.
+    card.classList.add("card", i.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+
+    //Container
+    let container = document.createElement("div");
+    container.classList.add("container");
+
+    //product name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+
+    //price
+    let price = document.createElement("h6");
+    price.innerText = "$" + i.price;
+    container.appendChild(price);
+
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+
+}
+
+// parameter passed from button (Parameter same as category)
+function filterProduct(Value){
+  //Button class code
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    //check if value equals innerText
+    if(Value.toUpperCase() == button.innerText.toUpperCase()){
+      button.classList.add(active);
+    }else{
+      button.classList.remove("active");
+    }
+  });
+
+  //select all cards
+  let elements = document/querySelectorAll(".card");
+  //loop through all cards
+  elements.forEach((element) =>{
+    //display all cards on 'all' button click
+    if (Value == "all"){
+      element.classList.remove("hide");
+    }else{
+      //check if element contains category class
+      if (element.classList.contains(value)){
+        //display element based on category
+        element.classList.remove("hide");
+      }
+      else{
+       //hide other elements
+       element.classList.add("hide");
+    } 
+    }
+  });
+}
+
+// Search button click
+document.getElementById("search").addEventListener
+("click", () => {
+  //initializations
+})
+
+
+//initially display all products
+window.onload = () => {
+  filterProduct("all");
 }
