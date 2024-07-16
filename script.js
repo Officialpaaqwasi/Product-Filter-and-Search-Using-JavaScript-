00428c9f5ef9a06677409911d6766b5fb5391b5c
officialpaaqwasi <iklutse@st.ug.edu.gh>
2024-07-16 14:52:05 +0000

    16/07/24 phase2

--- script.js
+++ script.js
@@ -125,7 +125,23 @@ function filterProduct(Value){
 document.getElementById("search").addEventListener
 ("click", () => {
   //initializations
-})
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+
+
+  //loop through all elements
+  elements.forEach((element,index) =>{
+    //check if text includes the search value
+    if(element.innerText.includes(searchInput.toUpperCase())){
+      //display matching card
+      cards[index].classList.remove("hide");
+    }else{
+      //hide others
+      cards[index].classList.add(hide);
+    }
+  });
+});
 
 
 //initially display all products
